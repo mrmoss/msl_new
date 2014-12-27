2be020a5b1f2c2ba62f0c5d77a8b5e655c4c9a72
Your Name <you@example.com>
2014-12-26 22:25:36 -0900

    Added some vector and matrix headers.

--- testing/compile.sh
+++ testing/compile.sh
@@ -3,4 +3,5 @@
 #g++ client.cpp ../socket.cpp ../string.cpp ../time.cpp -o client -O -Wall -std=c++11 -ggdb #-lWs2_32
 #g++ udp_client.cpp ../socket.cpp ../string.cpp ../time.cpp -o udp_client -O -Wall -std=c++11 -ggdb #-lWs2_32
 #g++ test.cpp ../socket.cpp -o test -O -Wall -std=c++11 -ggdb
-g++ crypto_test.cpp ../crypto.cpp ../string.cpp -o crypto_test -O -Wall -std=c++11 -lcrypto -ggdb
+#g++ crypto_test.cpp ../crypto.cpp ../string.cpp -o crypto_test -O -Wall -std=c++11 -lcrypto -ggdb
+g++ vec_test.cpp -o vec_test -O -Wall -std=c++11 -lcrypto -ggdb
