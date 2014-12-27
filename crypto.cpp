65efbe4acbe6463ffd564bdf1b0579b4af1a5a7d
Your Name <you@example.com>
2014-12-26 22:05:55 -0900

    ...

--- crypto.cpp
+++ crypto.cpp
@@ -69,6 +69,40 @@ bool encrypt_rsa(const std::string& plain,const std::string& key,std::string& ci
 	return encrypt_rsa(plain.c_str(),plain.size(),key,cipher);
 }
 
+bool encrypt_aes256(const void* plain,const size_t size,const std::string& key,const std::string& iv,std::string& cipher)
+{
+	bool success=false;
+	ERR_load_crypto_strings();
+	OpenSSL_add_all_algorithms();
+	EVP_CIPHER_CTX* ctx=EVP_CIPHER_CTX_new();
+	uint8_t* temp_data=new uint8_t[(size/16+1)*16];
+
+	int temp_length;
+	int temp_unaligned_length;
+
+	if(ctx!=nullptr&&EVP_CIPHER_CTX_set_padding(ctx,1)!=0&&
+		EVP_EncryptInit_ex(ctx,EVP_aes_256_cbc(),nullptr,(uint8_t*)key.c_str(),(uint8_t*)iv.c_str())!=0&&
+		EVP_EncryptUpdate(ctx,temp_data,&temp_length,(uint8_t*)plain,size)!=0&&
+		EVP_EncryptFinal_ex(ctx,temp_data+temp_length,&temp_unaligned_length)!=0)
+	{
+		cipher=std::string((char*)temp_data,temp_length+temp_unaligned_length);
+		success=true;
+	}
+
+	delete[] temp_data;
+	EVP_CIPHER_CTX_free(ctx);
+	ERR_free_strings();
+	EVP_cleanup();
+	ERR_remove_state(0);
+	CRYPTO_cleanup_all_ex_data();
+	return success;
+}
+
+bool encrypt_aes256(const std::string& plain,const std::string& key,const std::string& iv,std::string& cipher)
+{
+	return encrypt_aes256(plain.c_str(),plain.size(),key,iv,cipher);
+}
+
 bool decrypt_rsa(const void* cipher,const size_t size,const std::string& key,std::string& plain)
 {
 	bool success=true;
@@ -115,40 +149,6 @@ bool decrypt_rsa(const void* cipher,const size_t size,const std::string& key,std
 bool decrypt_rsa(const std::string& cipher,const std::string& key,std::string& plain)
 {
 	return decrypt_rsa(cipher.c_str(),cipher.size(),key,plain);
-}
-
-bool encrypt_aes256(const void* plain,const size_t size,const std::string& key,const std::string& iv,std::string& cipher)
-{
-	bool success=false;
-	ERR_load_crypto_strings();
-	OpenSSL_add_all_algorithms();
-	EVP_CIPHER_CTX* ctx=EVP_CIPHER_CTX_new();
-	uint8_t* temp_data=new uint8_t[(size/16+1)*16];
-
-	int temp_length;
-	int temp_unaligned_length;
-
-	if(ctx!=nullptr&&EVP_CIPHER_CTX_set_padding(ctx,1)!=0&&
-		EVP_EncryptInit_ex(ctx,EVP_aes_256_cbc(),nullptr,(uint8_t*)key.c_str(),(uint8_t*)iv.c_str())!=0&&
-		EVP_EncryptUpdate(ctx,temp_data,&temp_length,(uint8_t*)plain,size)!=0&&
-		EVP_EncryptFinal_ex(ctx,temp_data+temp_length,&temp_unaligned_length)!=0)
-	{
-		cipher=std::string((char*)temp_data,temp_length+temp_unaligned_length);
-		success=true;
-	}
-
-	delete[] temp_data;
-	EVP_CIPHER_CTX_free(ctx);
-	ERR_free_strings();
-	EVP_cleanup();
-	ERR_remove_state(0);
-	CRYPTO_cleanup_all_ex_data();
-	return success;
-}
-
-bool encrypt_aes256(const std::string& plain,const std::string& key,const std::string& iv,std::string& cipher)
-{
-	return encrypt_aes256(plain.c_str(),plain.size(),key,iv,cipher);
 }
 
 bool decrypt_aes256(const void* cipher,const size_t size,const std::string& key,const std::string& iv,std::string& plain)
