bbae34149a3c5be58bf7415ac30a4968885410ba
Your Name <you@example.com>
2015-01-20 14:04:29 -0900

    Added webserver support (wrapper around mongoose).

---
+++ testing/webserver_test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "../time.hpp"
+#include "../webserver.hpp"
+
+bool client_func(mg_connection connection,enum mg_event event)
+{
+	if(event==MG_AUTH)
+		return true;
+	if(event!=MG_REQUEST)
+		return false;
+
+	std::cout<<connection.uri<<std::endl;
+	return false;
+}
+
+int main()
+{
+	msl::webserver_t test(client_func,"0.0.0.0:8080","web");
+	test.open();
+
+	if(test.good())
+		std::cout<<":)"<<std::endl;
+	else
+		std::cout<<":("<<std::endl;
+
+	while(test.good())
+		msl::delay_ms(1);
+
+	return 0;
+}
