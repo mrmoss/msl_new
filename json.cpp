4c80a5db3c498b4681edab3e3c62ca510436ddbb
Your Name <you@example.com>
2015-04-09 11:08:32 -0800

    Removed whitespace at end of json serialization...

--- json.cpp
+++ json.cpp
@@ -5,7 +5,12 @@
 std::string msl::serialize(const msl::json& json)
 {
 	Json::FastWriter writer;
-	return writer.write(json);
+	std::string ret=writer.write(json);
+
+	while(ret.size()>0&&isspace(ret[ret.size()-1])!=0)
+		ret.pop_back();
+
+	return ret;
 }
 
 msl::json msl::deserialize(const std::string& str)
