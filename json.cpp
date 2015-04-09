9a74ea06ab99109ac6a284c905d4e38ffb0e0eb8
Your Name <you@example.com>
2015-04-08 21:29:19 -0800

    Changed json library...

--- json.cpp
+++ json.cpp
@@ -1,19 +1,12 @@
 #include "json.hpp"
 
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
-
-msl::json msl::string_to_json(const std::string& str)
+std::string msl::serialize(const msl::json& json)
 {
-	msl::json json;
-	json.Parse(str.c_str());
-	return json;
+	Json::FastWriter writer;
+	return writer.write(json);
 }
 
-std::string msl::json_to_string(const msl::json& json)
+msl::json msl::deserialize(const std::string& str)
 {
-	rapidjson::StringBuffer buffer;
-	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-	json.Accept(writer);
-	return std::string(buffer.GetString());
+	return msl::json(str);
 }
--- json.hpp
+++ json.hpp
@@ -1,14 +1,14 @@
 #ifndef MSL_C11_JSON_HPP
 #define MSL_C11_JSON_HPP
 
-#include "rapidjson/document.h"
+#include "jsoncpp/json.h"
+#include <string>
 
 namespace msl
 {
-	typedef rapidjson::Document json;
-
-	json string_to_json(const std::string& str);
-	std::string json_to_string(const json& json);
+	typedef Json::Value json;
+	std::string serialize(const json& json);
+	json deserialize(const std::string& str);
 }
 
 #endif
